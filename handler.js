0d6e0102bce7aaef302b418f62b24047f60e73e9
Sanjai Ragul Murugesh <sanjairagulm@gmail.com>
2025-04-10 13:53:46 +0530

    Updated Command v0.3

--- handler.js
+++ handler.js
@@ -21,7 +21,7 @@ module.exports.registerUser = async (event) => {
     await dynamoDb.put(params).promise();
     return {
       statusCode: 201,
-      body: JSON.stringify({ message: "User registered successfully😀" }),
+      body: JSON.stringify({ message: "User registered successfully" }),
     };
   } catch (err) {
     console.error(err);
